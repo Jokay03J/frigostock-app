da773cd247aa8fab494dd987a96558c99c2bbdc6
jokay03J <jokay03j@gmail.com>
2022-06-08 15:31:41 +0200

    update main page
    *create function for change theme

--- lib/main.dart
+++ lib/main.dart
@@ -17,26 +17,67 @@ Future<void> main() async {
 
   SystemChrome.setPreferredOrientations(
       [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]).then((_) {
-    runApp(
-      MaterialApp(
-        home: const Home(title: "Frigostock - Accueil"),
-        theme: ThemeData(
-          primarySwatch: Colors.red,
-        ),
-        darkTheme: ThemeData(brightness: Brightness.dark),
-        themeMode: ThemeMode.system,
-        routes: <String, WidgetBuilder>{
-          '/newProduct': (BuildContext context) =>
-              const NewProductPage(title: "Frigostock - Nouveaux Produit"),
-          '/account': (BuildContext context) =>
-              const Account(title: "Frigostock - Compte"),
-          '/login': (BuildContext context) =>
-              const Login(title: "Frigostock - Connexion"),
-          '/register': (BuildContext context) =>
-              const Register(title: "Frigostock - Inscription"),
-        },
-        title: "Frigostock",
-      ),
-    );
+    runApp(const App());
   });
+}
+
+class App extends StatefulWidget {
+  const App({Key? key}) : super(key: key);
+
+  @override
+  _App createState() => _App();
+
+  static _App? of(BuildContext context) =>
+      context.findAncestorStateOfType<_App>();
+}
+
+class _App extends State<App> {
+  ThemeMode _themeMode = ThemeMode.system;
+  int SelectedIndex = 0;
+
+  Future<void> changeTheme(int themeMode) async {
+    switch (themeMode) {
+      case 0:
+        setState(() {
+          SelectedIndex = 0;
+          _themeMode = ThemeMode.system;
+        });
+        break;
+      case 1:
+        setState(() {
+          SelectedIndex = 1;
+          _themeMode = ThemeMode.dark;
+        });
+        break;
+      case 2:
+        setState(() {
+          SelectedIndex = 2;
+          _themeMode = ThemeMode.light;
+        });
+        break;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: const Home(title: "Frigostock - Accueil"),
+      theme: ThemeData(
+        primarySwatch: Colors.red,
+      ),
+      darkTheme: ThemeData(brightness: Brightness.dark),
+      themeMode: _themeMode,
+      routes: <String, WidgetBuilder>{
+        '/newProduct': (BuildContext context) =>
+            const NewProductPage(title: "Frigostock - Nouveaux Produit"),
+        '/account': (BuildContext context) =>
+            const Account(title: "Frigostock - Compte"),
+        '/login': (BuildContext context) =>
+            const Login(title: "Frigostock - Connexion"),
+        '/register': (BuildContext context) =>
+            const Register(title: "Frigostock - Inscription"),
+      },
+      title: "Frigostock",
+    );
+  }
 }
