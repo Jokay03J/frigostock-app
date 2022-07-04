5a7678da0e11fd265f547f0bea22b47f0aa46557
jokay03J <jokay03j@gmail.com>
2022-07-04 11:00:01 +0200

    switch next.js to react js

--- docs/_next/static/n0oRD2LgyRhK1ZIfvQOTp/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/3791aa3696ec00ef.css","static/chunks/pages/index-786ff2a27a8a8416.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
